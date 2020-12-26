ceef394ffdf908cf73f41d731daaa7a671a37376
WingLim <643089849@qq.com>
2020-12-26 16:08:01 +0800

    feat: create .dbinited to prevent reinitialization

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+while ! nc -z $DBHOST 3306; do
+    sleep 5s
+    echo "Waiting for MariaDB..."
+done
+
+dbinited_path="/etc/kea/.dbinited"
+
+if [ ! -f $dbinited_path ];then
+
 DATABASE=$(jq -r '.Dhcp4."lease-database"' /etc/kea/kea-dhcp4.conf)
 TYPE=$(echo "$DATABASE" | jq -r '.type')
 DBHOST=$(echo "$DATABASE" | jq -r '.host')
@@ -9,20 +18,14 @@ NAME=$(echo "$DATABASE" | jq -r '.name')
 
 echo "Using $TYPE as lease database"
 
-while ! nc -z $DBHOST 3306; do
-    sleep 5s
-    echo "Waiting for MariaDB..."
-done
-
-if [ -z $DBINITED ];then
 case "$TYPE" in
     "mysql"|"pgsql")
         kea-admin db-init ${TYPE} -u ${USER} -p ${PASS} -n ${NAME} -h ${DBHOST}
-        export DBINITED=1
+        touch $dbinited_path
     ;;
     "cql")
         kea-admin db-init ${TYPE} -n ${NAME}
-        export DBINITED=1
+        touch $dbinited_path
     ;;
 esac
 fi
